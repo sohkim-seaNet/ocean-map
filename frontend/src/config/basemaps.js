736d47e8677ee20eadca86924d756f85175e6e68
sohkim-seaNet <sohkim@sea-net.co.kr>
2025-12-22 11:03:18 +0900

    Refactor: ETOPO 베이스맵 렌더링 방식 변경

--- frontend/src/config/basemaps.js
+++ frontend/src/config/basemaps.js
@@ -10,11 +10,24 @@ export const basemaps = {
     },
     ETOPO: {
         tiles: [
+        /*
             '/geoserver/ocean/wms?' +
             'service=WMS&request=GetMap&version=1.1.1&' +
             'layers=ocean:etopo2022_30s&' +
             'styles=&bbox={bbox-epsg-3857}&' +
             'width=256&height=256&srs=EPSG:3857&format=image/png&transparent=false'
+        */
+            '/geoserver/gwc/service/wmts?' +
+            'SERVICE=WMTS&' +
+            'REQUEST=GetTile&' +
+            'VERSION=1.0.0&' +
+            'LAYER=ocean:ETOPO_ice_surface_elevation_15s&' +
+            'STYLE=&' +
+            'TILEMATRIXSET=EPSG:900913&' +
+            'TILEMATRIX=EPSG:900913:{z}&' +
+            'TILEROW={y}&' +
+            'TILECOL={x}&' +
+            'FORMAT=image/jpeg'
         ]
     }
 };
